6e0b4a9702957c46adf48e49dec1a92110a16122
zhshy <18302010080@fudan.edu.cn>
2020-07-13 17:05:57 +0800

    v1.1 [front] import element-ui

--- project-frontend/src/main.js
+++ project-frontend/src/main.js
@@ -3,13 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import ElementUI from 'element-ui'
+import '../node_modules/element-ui/lib/theme-chalk/index.css'
+import axios from '../node_modules/axios'
+import qs from '../node_modules/qs'
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  axios,
+  qs,
   components: { App },
   template: '<App/>'
 })
