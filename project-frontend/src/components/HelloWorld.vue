5c84bb5f58fc9149ccaecbaa378e484eeda899c7
zhshy <18302010080@fudan.edu.cn>
2020-07-13 19:16:03 +0800

    v1.2 [front] implement login page

--- project-frontend/src/components/HelloWorld.vue
+++ project-frontend/src/components/HelloWorld.vue
@@ -1,17 +1,86 @@
-<!--修改模板如下-->
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-        <el-button type="primary" @click="visible = true">Button</el-button>
-            <el-dialog :visible.sync="visible" title="Hello world">
-            <p>Try Element</p>
-        </el-dialog>
-        <br><br>
-        <el-button type="primary" @click="getRequest">Get测试</el-button>
-        <el-button type="primary" @click="postRequest">Post测试</el-button>
-    <h2>Ecosystem</h2>
-  </div>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 
@@ -20,7 +89,6 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      visible: false,
       msg: 'Welcome to Your Vue.js App'
     }
   }
---
+++ project-frontend/src/pages/Login.vue
@@ -0,0 +1,135 @@
+<template>
+    <el-container>
+        <el-header>
+            Login
+        </el-header>
+        <el-main>
+            <el-row :gutter="20">
+                <el-col :span="6" :offset="9">
+                    <el-form 
+                    @submit.native.prevent
+                    status-icon
+                    :model="loginForm"
+                    :rules="rules"
+                    ref="loginForm"
+                    label-position="left"
+                    label-width="0px"
+                    v-loading="loading"
+                    >
+                        <el-form-item prop="username" >
+                            <el-input
+                                type="text"
+                                v-model="loginForm.username"
+                                auto-complete="off"
+                                placeholder="Username"
+                            ></el-input>
+                        </el-form-item>
+                        <el-form-item prop="password" >
+                            <el-input
+                                type="password"
+                                show-password
+                                v-model="loginForm.password"
+                                auto-complete="off"
+                                placeholder="Password"
+                            ></el-input>
+                        </el-form-item>
+                            
+                        <el-form-item size="medium">
+                            <el-button
+                            native-type="submit"
+                            :disabled ="isDisabled"
+                            size="medium"
+                            type="primary"
+                            style="width:100% "
+                            v-on:click="login"
+                            >Sign In</el-button>
+                        </el-form-item>
+                        <el-form-item size="medium">
+                            <el-button
+                            plain
+                            style="width:100%"
+                            @click="resetForm('loginForm')"
+                            >Reset
+                            </el-button>
+                        </el-form-item>
+                    </el-form>
+                </el-col>
+            </el-row>
+        </el-main>
+    </el-container>
+</template>
+
+<script>
+  export default {
+    name: "Login",
+    data() {
+      return {
+        loginForm: {
+          username: "",
+          password: ""
+        },
+        rules: {
+          username: [
+            {required:true,message:"Username is required",blur:"change"},
+            {pattern:/^[a-zA-Z-][a-zA-Z0-9-_]{4,31}$/,message:"Invalid username",blur:"change"}
+          ],
+          password: [
+            {required:true, message:"Password is required", blur:"change"},
+            {pattern:/^[\w-]{6,32}$/, message:"Invalid password",blur:"change"},
+          ]
+        },
+        loading: false,
+      };
+    },
+    computed:{
+      isDisabled(){
+        return 
+        !(/^[a-zA-Z-][a-zA-Z0-9-_]{4,31}$/.test(this.loginForm.username)
+         && /^[\w-]{6,32}$/.test(this.loginForm.password));
+      }
+    },
+    methods: {
+      login() {
+        // Turn to loading mode when the form is submitted,and come back when getting response
+        this.loading = true;
+        this.$axios
+          .post("/login", {
+            username: this.loginForm.username,
+            password: this.loginForm.password
+          })
+          .then(resp => {
+            if (resp.status === 200 && resp.data.hasOwnProperty("token")) {
+              //Save token
+              this.$store.commit("login", resp.data);
+              this.$message({
+                dangerouslyUseHTMLString: true,
+                type:'success',
+                message: '<strong style="color:teal">Welcome back!</strong>',
+                center:true
+              });
+              this.$router.replace({ path: "/" });
+            } else {
+              this.errorNotification();
+              this.loading = false;
+            }
+          })
+          .catch(error => {
+            console.log(error);
+            this.errorNotification();
+            this.loading = false;
+          });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      },
+      errorNotification(){
+        this.$notify({
+          type:'error',
+          dangerouslyUseHTMLString: true,
+          title: 'Login error',
+          message: '<strong style="color:teal">Please check your username and password or try again later!</strong>'
+        });
+      },
+    }
+  }
+</script>
