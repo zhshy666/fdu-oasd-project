6c453d0eae5e1439e356de1c37bf52553fdeea27
zhshy <18302010080@fudan.edu.cn>
2020-07-14 12:17:27 +0800

    v1.7 [frontend] import layui and simpilfy page 'login'

--- project-frontend/src/pages/Login.vue
+++ project-frontend/src/pages/Login.vue
@@ -1,62 +1,79 @@
 <template>
+<div>
+<div class="layui-container">
+    <div style="height: 150px"></div>
+</div>
     <el-container>
-        <el-header>
-            Login
+        <el-header height="50%">
+          <div class="layui-col-md4 layui-col-md-offset4">
+              <h1>Login</h1>
+          </div>
         </el-header>
         <el-main>
-            <el-row :gutter="20">
-                <el-col :span="6" :offset="9">
-                    <el-form 
-                    @submit.native.prevent
-                    status-icon
-                    :model="loginForm"
-                    :rules="rules"
-                    ref="loginForm"
-                    label-position="left"
-                    label-width="0px"
-                    v-loading="loading"
-                    >
-                        <el-form-item prop="username" >
-                            <el-input
-                                type="text"
-                                v-model="loginForm.username"
-                                auto-complete="off"
-                                placeholder="Username"
-                            ></el-input>
-                        </el-form-item>
-                        <el-form-item prop="password" >
-                            <el-input
-                                type="password"
-                                show-password
-                                v-model="loginForm.password"
-                                auto-complete="off"
-                                placeholder="Password"
-                            ></el-input>
-                        </el-form-item>
-                            
-                        <el-form-item size="medium">
-                            <el-button
-                            native-type="submit"
-                            :disabled ="isDisabled"
-                            size="medium"
-                            type="primary"
-                            style="width:100% "
-                            v-on:click="login"
-                            >Sign In</el-button>
-                        </el-form-item>
-                        <el-form-item size="medium">
-                            <el-button
-                            plain
-                            style="width:100%"
-                            @click="resetForm('loginForm')"
-                            >Reset
-                            </el-button>
-                        </el-form-item>
-                    </el-form>
-                </el-col>
-            </el-row>
+            <el-col :span="6" :offset="9">
+                <el-form 
+                  @submit.native.prevent
+                  status-icon
+                  :model="loginForm"
+                  :rules="rules"
+                  ref="loginForm"
+                  label-position="left"
+                  label-width="0px"
+                  v-loading="loading"
+                >
+                    <el-form-item prop="username" >
+                        <el-input
+                          type="text"
+                          v-model="loginForm.username"
+                          auto-complete="off"
+                          placeholder="Username"
+                        ></el-input>
+                    </el-form-item>
+                    <el-form-item prop="password" >
+                        <el-input
+                          type="password"
+                          show-password
+                          v-model="loginForm.password"
+                          auto-complete="off"
+                          placeholder="Password"
+                        ></el-input>
+                    </el-form-item>
+
+                    <el-form-item size="medium">
+                        <button v-if="isDisabled"
+                          type="button"
+                          class="layui-btn layui-btn-disabled"
+                          native-type="submit"
+                          disabled
+                          size="medium"
+                          style="width:100% "
+                          v-on:click="login">
+                            Login
+                        </button>
+                        <button v-else
+                          type="button"
+                          class="layui-btn"
+                          native-type="submit"
+                          size="medium"
+                          style="width:100% "
+                          v-on:click="login">
+                            Login
+                        </button>
+                    </el-form-item>
+                    <el-form-item size="medium">
+                        <button 
+                          type="button"
+                          style="width:100%"
+                          @click="resetForm('loginForm')"
+                          class="layui-btn layui-btn-primary">
+                            Reset
+                        </button>
+                    </el-form-item>
+                </el-form>
+            </el-col>
         </el-main>
     </el-container>
+</div>
 </template>
 
 <script>
@@ -71,11 +88,9 @@
         rules: {
           username: [
             {required:true,message:"Username is required",blur:"change"},
-            // {pattern:/^[a-zA-Z-][a-zA-Z0-9-_]{4,31}$/,message:"Invalid username",blur:"change"}
           ],
           password: [
             {required:true, message:"Password is required", blur:"change"},
-            // {pattern:/^[\w-]{6,32}$/, message:"Invalid password",blur:"change"},
           ]
         },
         loading: false,
@@ -83,8 +98,7 @@
     },
     computed:{
       isDisabled(){
-          return false
-        // return !(/^[a-zA-Z-][a-zA-Z0-9-_]{4,31}$/.test(this.loginForm.username) && /^[\w-]{6,32}$/.test(this.loginForm.password));
+        return !(this.loginForm.username && this.loginForm.password);
       }
     },
     methods: {
@@ -100,11 +114,12 @@
             if (resp.status === 200 && resp.data.hasOwnProperty("token")) {
               //Save token
               this.$store.commit("login", resp.data);
-              this.$message({
+              this.$notify({
                 dangerouslyUseHTMLString: true,
                 type:'success',
-                message: '<strong style="color:teal">Welcome back!</strong>',
-                center:true
+                title: 'Login success',
+                showClose: false,
+                message: '<strong style="color:teal"Login successfully!</strong>',
               });
               this.$router.replace({ path: "/" });
             } else {
@@ -125,6 +140,7 @@
         this.$notify({
           type:'error',
           dangerouslyUseHTMLString: true,
+          showClose: false,
           title: 'Login error',
           message: '<strong style="color:teal">Please check your username and password or try again later!</strong>'
         });
